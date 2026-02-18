11b996b94b93b446dcb00bb72d99cf8237440a69
Abdihakim Mohamed <abdihakma0@gmail.com>
2026-02-18 13:58:14 +0300

    Customize admin site with Hakimu branding

--- blog/admin.py
+++ blog/admin.py
@@ -3,24 +3,41 @@ from .models import Category, Post, Comment
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'created_at']
+    list_display = ('name', 'slug', 'created_at')
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    list_filter = ('created_at',)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'category', 'status', 'views', 'created_at']
-    list_filter = ['status', 'category', 'created_at']
-    search_fields = ['title', 'content']
+    list_display = ('title', 'author', 'category', 'status', 'views', 'created_at')
+    list_filter = ('status', 'category', 'created_at', 'author')
+    search_fields = ('title', 'content', 'excerpt')
     prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ['author']
+    raw_id_fields = ('author',)
     date_hierarchy = 'created_at'
-    ordering = ['-created_at']
+    ordering = ('-created_at',)
+    
+    fieldsets = (
+        ('Post Information', {
+            'fields': ('title', 'slug', 'author', 'category')
+        }),
+        ('Content', {
+            'fields': ('content', 'excerpt', 'featured_image')
+        }),
+        ('Status', {
+            'fields': ('status', 'views')
+        }),
+        ('Dates', {
+            'fields': ('published_at',)
+        }),
+    )
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'post', 'is_approved', 'created_at']
-    list_filter = ['is_approved', 'created_at']
-    search_fields = ['name', 'email', 'content']
+    list_display = ('name', 'post', 'email', 'is_approved', 'created_at')
+    list_filter = ('is_approved', 'created_at')
+    search_fields = ('name', 'email', 'content')
     actions = ['approve_comments']
     
     def approve_comments(self, request, queryset):
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+# Customize Admin Site
+admin.site.site_header = "Hakimu Official Administration"
+admin.site.site_title = "Hakimu Admin Portal"
+admin.site.index_title = "Welcome to Hakimu Dashboard"
+admin.site.site_url = "https://hakimu.onrender.com"
+
+# Customize User Admin
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('-date_joined',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+# Unregister default User admin and register custom one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+# Add custom styling
+class Media:
+    css = {
+        'all': ('css/admin.css',)
+    }
